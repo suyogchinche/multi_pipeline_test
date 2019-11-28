225c95e0bc06e58ffa2a170d89756c2ad12f7ad3
schinche <schinche@oracle.com>
2019-11-28 09:41:07 +0530

    Updated groovy file

--- vars/getBuildVersion.groovy
+++ vars/getBuildVersion.groovy
@@ -1,17 +1,16 @@
 def call() {
 
-   tag_id = sh(script: "git describe --abbrev=0", returnStdout: true)?.trim()
+    tag_id = sh(script: "git describe --abbrev=0", returnStdout: true)?.trim()
 
-   env.TAG_NAME = tag_id
+    env.TAG_NAME = tag_id
 
-   if ( BRANCH_NAME == 'master' || BRANCH_NAME == 'hostfix' || BRANCH_NAME == 'release' ) {
-       echo "master-hotfix-release"
-       return "${TAG_NAME}"
-   } else if ( BRANCH_NAME == 'development' || ( BRANCH_NAME =~ /feature/ &&  TAG_NAME =~ /release/ ) ) {
-       echo "development-feature-*"
-       return "${TAG_NAME}-SNAPSHOT"
-   } else {
-       return null
-   }
-
+    if ( BRANCH_NAME == 'master' || BRANCH_NAME == 'hostfix' || BRANCH_NAME == 'release' ) {
+        echo "master-hotfix-release"
+        return "${TAG_NAME}"
+    } else if ( BRANCH_NAME == 'develop' || ( BRANCH_NAME =~ /feature/ &&  TAG_NAME =~ /release/ ) ) {
+        echo "development-feature-*"
+        return "${TAG_NAME}-SNAPSHOT"
+    } else {
+        return null
+    }
 }
